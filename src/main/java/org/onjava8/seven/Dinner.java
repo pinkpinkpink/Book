61ce3c7645b6c4a0fd077227779f826d7372d0e6
wangyifan <wyf16601291351@163.com>
2020-07-14 09:44:34 +0800

    package public private

---
+++ src/main/java/org/onjava8/seven/Dinner.java
@@ -0,0 +1,11 @@
+package org.onjava8.seven;
+
+/**
+ * 描述:
+ *
+ * @author wangyifan
+ * @create 2020-07-14 9:02 上午
+ */
+public class Dinner {
+
+}
---
+++ src/main/java/org/onjava8/seven/Range.java
@@ -0,0 +1,11 @@
+package org.onjava8.seven;
+
+/**
+ * 描述:
+ *
+ * @author wangyifan
+ * @create 2020-07-14 8:52 上午
+ */
+public class Range {
+
+}
